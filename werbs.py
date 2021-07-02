c418f50e3fd732aee9891245604114318ffd1677
Alexander Vynnyk <cosmonaut.ok@zoho.com>
2021-07-02 18:09:43 +0300

    up

--- werbs.py
+++ werbs.py
@@ -1,36 +1,68 @@
 import subprocess
 
-class Werbs:
-    def __init__(out_suffix, pod_shell="/bin/bash", kctl_bin="kubectl"):
+class Werbs():
+    def __init__(self, out_suffix, pod_shell="/bin/bash", kctl_bin="kubectl", all_ns=False, trace=False):
         self.out_suffix = out_suffix
         self.shell = pod_shell
         self.kctl = kctl_bin
+        if all_ns:
+            self.all_ns = " --all-namespaces "
+        else:
+            self.all_ns = ""
+
+        self.trace = trace
 
 
-    def list_of(name):
+    def __pit(self, string):
+        if self.trace:
+            ns = self.current_namespace()
+            ctx = self.current_context()
+            print("cmd: `{}`; namespace: `{}`; context: `{}`".format(string, ns, ctx))
+
+
+    def current_namespace(self):
+        cmd = self.kctl + " config view --minify --output 'jsonpath={..namespace}'"
+        curr = subprocess.getoutput(cmd)
+        return curr
+
+
+    def current_context(self):
+        cmd = "kubectl config current-context" # get-contexts -o name
+        curr = subprocess.getoutput(cmd)
+        return curr
+
+
+    def list_of(self, name):
         cmd = "{} get {} -o name".format(self.kctl, name)
+        cmd += self.all_ns
+        self.__pit(cmd)
         lst = subprocess.getoutput(cmd)
         return lst
 
 
-    def print_of(name):
+    def print_of(self, name):
         cmd = "{} get {}".format(self.kctl, name)
-        cmd += out_suffix
+        cmd += self.all_ns
+        cmd += self.out_suffix
+        self.__pit(cmd)
         subprocess.call(cmd, shell=True)
 
 
-    def explain_of(name):
+    def explain_of(self, name):
         cmd = "{} explain {}".format(self.kctl, name)
+        self.__pit(cmd)
         subprocess.call(cmd, shell=True)
 
 
-    def describe_of(name):
+    def describe_of(self, name):
         cmd = "{} describe {}".format(self.kctl, name)
+        self.__pit(cmd)
         subprocess.call(cmd, shell=True)
 
 
-    def delete_of(name):
+    def delete_of(self, name):
         cmd = "{} delete {}".format(self.kctl, name)
+        self.__pit(cmd)
         subprocess.call(cmd, shell=True)
 
     
